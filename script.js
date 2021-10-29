52440c697c3246b0c1e0be79930c8bccfc6a9ae6
Johan Berglund <johan.berglund00@hotmail.com>
2021-10-29 17:09:52 +0200

    Uppgift-1

--- script.js
+++ script.js
@@ -3,16 +3,38 @@ function validate() {
     const lastName = document.getElementById('lastName')
 
     if (firstName.value.trim().length < 2) {
-        alert ('First and last name needs to contain at least 2 characters excluding spaces.')
+        document.getElementById('firstNameError').style.visibility="visible";
         firstName.style.border = "solid 2px red";
         return false;
     }
+
     else if (lastName.value.trim().length < 2) {
-        alert ('First and last name needs to contain at least 2 characters excluding spaces.')
+        document.getElementById('lastNameError').style.visibility="visible";
         lastName.style.border = "solid 2px red";
         return false;
     }
+
+    else if (email.value.trim().length < 8) {
+        document.getElementById('emailError').style.visibility="visible";
+        email.style.border = "solid 2px red";
+        return false;
+    }
+
     else {
         true;
     }
 }
+
+/*function validate() {
+    const email = document.getElementById('email').value;
+    const regex = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})(.[a-z]{2,8})$/;
+    
+    if (regex.test(text)) {
+        document.getElementById('emailError').style.visibility="visible";
+        email.style.border = "solid 2px red";
+    }
+
+    else {
+        true;
+    }
+}*/
